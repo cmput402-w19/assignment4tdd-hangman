9fe3e770789e5b9087b1589b38a9bc6160a1e212
Cosh_ <cosh@slouple.com>
2019-02-25 02:09:15 -0700

    RED Added Body Parts Error Tests

--- client/tests/bodyparts_test.py
+++ client/tests/bodyparts_test.py
@@ -21,6 +21,26 @@ class BodyPartTestCase(unittest.TestCase):
         self.body.add_part(RightLeg())
         self.assertIs(self.body.is_dead(), True)
 
+    def test_duplicate_body_parts(self):
+        self.body.add_part(Noose())
+        self.body.add_part(Head())
+        self.body.add_part(Torso())
+        self.body.add_part(LeftLeg())
+        self.body.add_part(LeftLeg())
+        self.body.add_part(LeftLeg())
+        self.body.add_part(LeftLeg())
+        self.assertIs(self.body.is_dead(), False)
+
+    def test_invalid_body_parts(self):
+        self.body.add_part(Noose())
+        self.body.add_part(Head())
+        self.body.add_part(Torso())
+        self.body.add_part(1)
+        self.body.add_part(2)
+        self.body.add_part("--")
+        self.body.add_part("ABC")
+        self.assertIs(self.body.is_dead(), False)
+
     def test_body_count(self):
         self.body.add_part(Noose())
         self.assertIs(self.body.get_part_count(), 1)
@@ -36,6 +56,30 @@ class BodyPartTestCase(unittest.TestCase):
         self.assertIs(self.body.get_part_count(), 6)
         self.body.add_part(RightLeg())
         self.assertIs(self.body.get_part_count(), 7)
+
+    def test_duplicate_body_count(self):
+        self.body.add_part(Noose())
+        self.assertIs(self.body.get_part_count(), 1)
+        self.body.add_part(Head())
+        self.assertIs(self.body.get_part_count(), 2)
+        self.body.add_part(Head())
+        self.assertIs(self.body.get_part_count(), 2)
+        self.body.add_part(Noose())
+        self.assertIs(self.body.get_part_count(), 2)
+        self.body.add_part(RightArm())
+        self.assertIs(self.body.get_part_count(), 3)
+
+    def test_invalid_body_count(self):
+        self.body.add_part(Noose())
+        self.assertIs(self.body.get_part_count(), 1)
+        self.body.add_part(Head())
+        self.assertIs(self.body.get_part_count(), 2)
+        self.body.add_part(1)
+        self.assertIs(self.body.get_part_count(), 2)
+        self.body.add_part("LEG")
+        self.assertIs(self.body.get_part_count(), 2)
+        self.body.add_part(RightArm())
+        self.assertIs(self.body.get_part_count(), 3)
 
     def test_parts_drawable(self):
         parts = [Noose(), Head(), Torso(), LeftArm(), RightArm(), LeftLeg(), RightLeg()]

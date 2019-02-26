293c8484dd891ffc526d0ba6d6c7e0e2e4c1551a
Cosh_ <cosh@slouple.com>
2019-02-25 22:47:55 -0700

    GREEN Added Client Game Class

--- client/game.py
+++ client/game.py
@@ -1,41 +1,86 @@
 import sys
 import asyncio
 import websockets
+import json
+from client.body import *
+from client.bodyparts import *
+import threading
 
 class Game:
     def __init__(self, ip, username):
-        pass
+        self.ip = ip
+        self.username = username
+        self._body = Body()
+        self._parts = [Noose(), Head(), Torso(), LeftArm(), RightArm(), LeftLeg(), RightLeg()]
+        self._partIndex = 0
+        self._incorrectCount = 0
+        self._inputThread = threading.Thread(target=self.read_input, args=[])
 
-    def start(self):
-        pass
+    async def start(self):
+        async with websockets.connect("ws://" + self.ip + ":80") as self.websocket:
+            self.send_username()
+            self._inputThread.start()
+            while True:
+                message = await self.websocket.recv()
+                print(message)
 
+    def read_input(self):
+        while True:
+            letter = ""
+            while len(letter) != 1:
+                letter = input("Enter a letter:")
+            self.send_letter(letter)
+            
     def send_username(self):
-        pass
+        self.websocket.send(json.dumps({"type": "SET_NAME", "data": self.username}))
 
     def send_letter(self, letter):
-        pass
+        if len(letter) > 1:
+            return
+        if not letter.isalpha():
+            return
+        self.websocket.send(json.dumps({"type": "GUESS", "data": letter}))
 
-    def receive_message(self, messageDict):
-        pass
+    def receive_message(self, message):
+        messageDict = json.loads(message)
+        if messageDict["type"] == "UPDATE_STATE":
+            return self.receive_state(messageDict)
+        elif messageDict["type"] == "GAMEOVER":
+            return self.receive_termination(messageDict)
 
     def receive_state(self, messageDict):
-        pass
+        data = messageDict["data"]
+        result = "Word: " + data["word"] + "\n"
+        result += "Correct: " + str(data["correct"]) + "\n"
+        result += "Incorrect: " + str(data["incorrect"])
+        incorrectCount = len(data["incorrect"])
+        delta = incorrectCount - self._incorrectCount
+        for i in range(delta):
+            self.add_body_part()
+        return result
+
 
     def receive_termination(self, messageDict):
-        pass
-
-    def draw(self):
-        pass
+        data = messageDict["data"]
+        if data["win"] == "True":
+            result = "You Won!\n"
+        elif data["win"] == "False":
+            result = "You Lost!\n"
+        result += "Score: Correct:Incorrect\n"
+        for key, value in data["score"].items():
+            result += key + ": " + str(value["correct"]) + ":" + str(value["incorrect"])
+        return result
 
     def add_body_part(self):
-        pass
+        self._body.add_part(self._parts[self._partIndex])
+        self._partIndex += 1
 
     def get_body(self):
-        pass
+        return self._body
 
 
 if __name__ == '__main__':
-    ip = sys.argv[0]
-    username = sys.argv[1]
+    ip = sys.argv[1]
+    username = sys.argv[2]
     game = Game(ip, username)
     asyncio.get_event_loop().run_until_complete(game.start())

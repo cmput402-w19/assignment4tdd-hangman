770139bfef413a2bbdc3cd1aa7b67117260783b0
Kirsten Kwong <kskwong@ualberta.ca>
2019-02-25 23:45:34 -0700

    added server.py, renamed test files again

--- server/server.py
+++ server/server.py
@@ -1,36 +1,58 @@
 import websockets
 import asyncio
 import json
-import uuid
 
-from player import Player
-from game import Game
+from server.game import Game
+from server.player import Player
 
 PLAYERS = dict()
 game = None
 
 async def register(websocket):
     """Associates socket with a particular user"""
-    pass
+
+    PLAYERS[websocket] = Player()
+    print(PLAYERS)
+
+    await notify_players()
 
 async def unregister(websocket):
     """removes user from PLAYERS"""
-    pass
+    del PLAYERS[websocket]
+
 
 async def notify_players():
     """Sends message to allgame = None players in PLAYERS"""
-    pass
-
-async def update_game_state():
-    """"""
-    pass
+    if PLAYERS:
+        message = json.dumps(game.get_game_state())
+        print(PLAYERS.keys())
+        await asyncio.wait([player.send(message) for player in PLAYERS.keys()])
 
 async def handler(websocket, path):
     """handler function for incoming messages"""
-    pass
+    await register(websocket)
+    try:
+        await websocket.send(json.dumps(game.get_game_state()))
+        async for message in websocket:
+            data = json.loads(message)
+            print(data)
+            if data["type"] == "GUESS":
+                res = game.guess(data["data"])
+                if res:
+                    PLAYERS[websocket].increment_correct()
+                else:
+                    PLAYERS[websocket].increment_incorrect()
+
+                await notify_players()
+            elif data["type"] == "SET_NAME":
+                PLAYERS[websocket].set_name(data["data"])
+
+    finally:
+        await unregister(websocket)
 
 if __name__ == "__main__":
-    start_server = websockets.serve(hello, 'localhost', 8765)
+    game = Game()
+    start_server = websockets.serve(handler, 'localhost', 8080)
 
     asyncio.get_event_loop().run_until_complete(start_server)
     asyncio.get_event_loop().run_forever()
---
+++ server/tests/server_test.py
@@ -0,0 +1,45 @@
+from unittest import TestCase, mock
+import server.server as server
+import asyncio
+
+class TestServer(TestCase):
+    def setUp(self):
+        self.mockSocket = mock.MagicMock()
+        self.mockSocket.send = mock.MagicMock()
+
+        self.mockPlayer = mock.MagicMock()
+        self.mockPlayer.increment_correct = mock.MagicMock()
+        self.mockPlayer.increment_incorrect = mock.MagicMock()
+
+        self.mockGame = mock.MagicMock()
+        self.mockGame.guess = mock.MagicMock()
+        self.mockGame.guess.return_value = True
+        self.mockGame.get_game_state = mock.MagicMock()
+        self.mockGame.get_game_state.return_value = {"word": "_ e _ _ _ e _", "correct": ["e"], "incorrect": ["y"]}
+
+    # def test_register(self):
+    #     server.register(self.mockSocket)
+    #     self.assertIn(self.mockSocket, server.PLAYERS)
+
+    # def test_unregister(self):
+    #     server.PLAYERS = {self.mockSocket: self.mockPlayer}
+    #     server.unregister(self.mockSocket)
+
+    #     self.assertNotIn(self.mockSocket, server.PLAYERS)
+
+    # def test_notify_players(self):
+    #     server.PLAYERS = {self.mockSocket: self.mockPlayer}
+    #     server.notify_players()
+    #     self.assertTrue(self.mockSocket.send.called)
+
+    # def test_update_game_state_guess(self):
+    #     # server.game = self.mockGame
+
+    #     # server.handler(self.mockSocket)
+    #     pass
+
+    # def test_update_game_state_fail(self):
+    #     pass
+
+    # def test_handler(self):
+    #     pass
